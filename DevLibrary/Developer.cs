c53e77aba55e8d1f0dcc0c3d850407b01d15eeeb
Nate Kunsman <nkkuns@protonmail.com>
2021-09-26 16:27:19 -0400

    filled out Developer and DeveloperRepo

--- DevLibrary/Developer.cs
+++ DevLibrary/Developer.cs
@@ -4,10 +4,18 @@ namespace Developer
 {
     public class Developer
     {
-        public Developer(string firstName, string lastName, string idNum, bool Pluralsight)
+        public Developer() { }
+        public Developer(string name, string idNum, string teamName, bool pluralsight)
         {
-            FirstName = firstName;
-
+            Name = name;
+            IDNum = idNum;
+            TeamName = teamName;
+            Pluralsight = pluralsight;
         }
+        public string Name { get; set; }
+        public string IDNum { get; set; }
+        public string TeamName { get; set; }
+        public bool Pluralsight { get; set; }
+       
     }
 }
--- DevLibrary/DeveloperRepo.cs
+++ DevLibrary/DeveloperRepo.cs
@@ -6,7 +6,67 @@ using System.Threading.Tasks;
 
 namespace Developer
 {
-    class DeveloperRepo
+    //CRUD
+        //Create
+        //Read
+        //Update
+        //Delete
+    public class DeveloperRepo
     {
+        private readonly List<Developer> _developerDirectory = new List<Developer>();
+
+        //Create
+        public bool AddDeveloperToDirectory(Developer developer)
+        {
+            int startingCount = _developerDirectory.Count;
+
+            _developerDirectory.Add(developer);
+
+            bool wasAdded = (_developerDirectory.Count > startingCount) ? true : false;
+            return wasAdded;
+        }
+
+        //Read (GET)
+        public List<Developer> GetDevelopers()
+        {
+            return _developerDirectory;
+        }
+
+        //Get By Employee Name
+        public Developer GetDeveloperByName(string theDeveloperYouAreLookingFor)
+        {
+            foreach (Developer developer in _developerDirectory)
+            {
+                if (developer.Name.ToLower() == theDeveloperYouAreLookingFor.ToLower())
+                {
+                    return developer;
+                }
+            }
+                    return null;
+        }
+
+        //Update
+        public bool UpdateExistingDeveloper(Developer existingDeveloper, Developer newDeveloper)
+        {
+            if (existingDeveloper != null)
+            {
+                existingDeveloper.Name = newDeveloper.Name;
+                existingDeveloper.IDNum = newDeveloper.IDNum;
+                existingDeveloper.TeamName = newDeveloper.TeamName;
+                existingDeveloper.Pluralsight = newDeveloper.Pluralsight;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        //Delete
+        public bool DeleteDeveloper(Developer existingDeveloper)
+        {
+            bool result = _developerDirectory.Remove(existingDeveloper);
+            return result;
+        }
     }
 }
